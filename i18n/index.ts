5739e054c99850b5b08daab0b11d64dd3c238cd2
Gabriel Mochi <gmochi56@icloud.com>
2022-03-30 20:43:37 -0300

    configures i18n

---
+++ i18n/index.ts
@@ -0,0 +1,23 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguaDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(LanguaDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: require("./locales/en/translation.json"),
+      },
+      fr: {
+        translation: require("./locales/fr/translation.json"),
+      },
+    },
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;

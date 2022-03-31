fc4988b2697b428d42c83de260f055f2aedbe827
Gabriel Mochi <gmochi56@icloud.com>
2022-03-31 20:55:46 -0300

    customizes chackra components

---
+++ themes/components/index.ts
@@ -0,0 +1,8 @@
+import { ThemeExtension } from "themes";
+import Progress from "./progress";
+
+const components: ThemeExtension["components"] = {
+  Progress,
+};
+
+export default components;
--- themes/index.ts
+++ themes/index.ts
@@ -1,13 +1,14 @@
 import { extendTheme, Theme } from "@chakra-ui/react";
 import styles from "./styles";
 import fonts from "./fonts";
+import components from "./components";
 
 type DeepPartial<T> = {
   [P in keyof T]?: DeepPartial<T[P]>;
 };
 
 type Extensible<T> = {
-  [P in keyof T]: T[P] & { [key: string]: unknown };
+  [P in keyof T]: T[P] | { [key: string]: unknown };
 };
 
 export type ThemeExtension = DeepPartial<Extensible<Theme>>;
@@ -15,6 +16,7 @@ export type ThemeExtension = DeepPartial<Extensible<Theme>>;
 const extension: ThemeExtension = {
   fonts,
   styles,
+  components,
 };
 
 export const theme = extendTheme(extension);

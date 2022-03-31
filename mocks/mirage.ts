6a6390f597781471ef7501675b4519b25bfc3a2b
Gabriel Mochi <gmochi56@icloud.com>
2022-03-31 05:43:52 -0300

    adds mock apis related to authentication

--- mocks/mirage.ts
+++ mocks/mirage.ts
@@ -1,12 +1,19 @@
 import { createServer, Response } from "miragejs";
 import md5 from "md5";
 import Cookies from "js-cookie";
+import * as uuid from "uuid";
+
+const SESSION_COOKIE_NAME = "session";
 
 export const makeServer = ({ environment = "test" } = {}) => {
+  const userId = uuid.v4();
+
+  const isUserAuthenticated = (): boolean => {
+    return !!Cookies.get(SESSION_COOKIE_NAME);
+  };
+
   const server = createServer({
     environment,
-
-    models: {},
 
     routes() {
       this.namespace = "api/v1";
@@ -15,14 +22,29 @@ export const makeServer = ({ environment = "test" } = {}) => {
         return { status: "ok" };
       });
 
-      this.post("/users/auth", (schema, request) => {
+      this.get("/user", () => {
+        if (!isUserAuthenticated()) return new Response(401);
+
+        return {
+          id: userId,
+          name: "Gabriel Mochi",
+          email: "gmochi56@icloud.com",
+          createdAt: new Date(),
+        };
+      });
+
+      this.get("/auth", () => {
+        return { authenticated: isUserAuthenticated() };
+      });
+
+      this.post("/auth", (schema, request) => {
         const { username, password } = JSON.parse(request.requestBody);
 
         if (
           username === "gmochi56@icloud.com" &&
           md5(password) === "54ca4c4c1a1a31db487ec6e7e1fc3b61" // HelloWorld@123
         ) {
-          Cookies.set("session", new Date().getTime().toString(), {
+          Cookies.set(SESSION_COOKIE_NAME, new Date().getTime().toString(), {
             domain: "localhost",
             path: "/",
             expires: new Date().getTime() + 2 * 60 * 60 * 1000,
@@ -30,10 +52,18 @@ export const makeServer = ({ environment = "test" } = {}) => {
             secure: false,
           });
 
-          return new Response(200, undefined);
+          return new Response(200);
         }
 
         return new Response(401, undefined, {});
+      });
+
+      this.delete("/auth", () => {
+        if (!isUserAuthenticated()) return new Response(401);
+
+        Cookies.remove(SESSION_COOKIE_NAME);
+
+        return new Response(200);
       });
     },
   });

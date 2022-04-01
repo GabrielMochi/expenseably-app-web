0bfdd249bb12a37eda6991fff4989046358068e5
Gabriel Mochi <gmochi56@icloud.com>
2022-04-01 00:49:43 -0300

    improves themes

--- themes/components/index.ts
+++ themes/components/index.ts
@@ -1,8 +1,12 @@
 import { ThemeExtension } from "themes";
 import Progress from "./progress";
+import FormLabel from "./formLabel";
+import Input from "./input";
 
 const components: ThemeExtension["components"] = {
   Progress,
+  FormLabel,
+  Input,
 };
 
 export default components;
--- themes/index.ts
+++ themes/index.ts
@@ -2,6 +2,7 @@ import { extendTheme, Theme } from "@chakra-ui/react";
 import styles from "./styles";
 import fonts from "./fonts";
 import components from "./components";
+import colors from "./colors";
 
 type DeepPartial<T> = {
   [P in keyof T]?: DeepPartial<T[P]>;
@@ -14,6 +15,7 @@ type Extensible<T> = {
 export type ThemeExtension = DeepPartial<Extensible<Theme>>;
 
 const extension: ThemeExtension = {
+  colors,
   fonts,
   styles,
   components,

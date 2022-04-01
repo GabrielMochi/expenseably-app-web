f9cc51fc67f73bfeb130f070f963a717dad924c4
Gabriel Mochi <gmochi56@icloud.com>
2022-04-01 07:39:30 -0300

    updates api response of banks

--- services/getBanks.ts
+++ services/getBanks.ts
@@ -2,6 +2,6 @@ import { fetch } from "./fetch";
 import Bank from "interfaces/Bank";
 
 export const getBanks = async (): Promise<Bank[]> => {
-  const { data: banks } = await fetch.get<Bank[]>("banks");
-  return banks;
+  const { data } = await fetch.get<{ banks: Bank[] }>("banks");
+  return data.banks;
 };

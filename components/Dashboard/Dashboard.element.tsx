ea449a05ad15e30d52bad229fb42ed053c5d4b6b
Gabriel Mochi <gmochi56@icloud.com>
2022-04-01 15:43:23 -0300

    fixes dashboard grid layout

--- components/Dashboard/Dashboard.element.tsx
+++ components/Dashboard/Dashboard.element.tsx
@@ -2,9 +2,9 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 
 const DashboardElement = (): ReactElement => (
-  <Grid templateAreas="'banks transactions transactions'" gap="40px" h="300px">
-    <GridItem gridArea="banks"></GridItem>
-    <GridItem gridArea="transactions"></GridItem>
+  <Grid templateColumns="repeat(3, 1fr)" gap="40px" h="300px">
+    <GridItem></GridItem>
+    <GridItem colSpan={3}></GridItem>
   </Grid>
 );
 

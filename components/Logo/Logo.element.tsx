fe33db59c5881a98722c8f5f7d1bc76136ea054a
Gabriel Mochi <gmochi56@icloud.com>
2022-04-01 00:50:05 -0300

    allows props on Logo component

--- components/Logo/Logo.element.tsx
+++ components/Logo/Logo.element.tsx
@@ -1,18 +1,19 @@
-import { Box } from "@chakra-ui/react";
+import { Box, BoxProps } from "@chakra-ui/react";
 import React, { ReactElement } from "react";
 
-type Props = {
+type Props = BoxProps & {
   width?: string;
   height?: string;
 };
 
-const Logo = ({ width = "200px", height = "40px" }: Props): ReactElement => (
+const Logo = ({ width = "200px", height = "40px", ...props }: Props): ReactElement => (
   <Box
     bgImage="/img/expenseably-logo.svg"
     w={width}
     h={height}
     bgRepeat="no-repeat"
     bgSize="contain"
+    {...props}
   />
 );
 

820191377baaafd24399fdfc73c5fdc9d2c1c38b
Gabriel Mochi <gmochi56@icloud.com>
2022-04-01 04:18:59 -0300

    implements Header component

--- templates/Default.tsx
+++ templates/Default.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from "react";
 import SplashScreen from "components/SplashScreen";
+import Header from "components/Header";
 
 const Default = (): ReactElement => {
   return (
     <>
+      <Header />
       <SplashScreen />
     </>
   );

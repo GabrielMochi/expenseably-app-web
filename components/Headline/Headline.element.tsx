30d647be1e878ff1801079350002a1d8226ac288
Gabriel Mochi <gmochi56@icloud.com>
2022-04-01 23:11:19 -0300

    adds h5 to headline component

--- components/Headline/Headline.element.tsx
+++ components/Headline/Headline.element.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from "react";
 import { Heading, HeadingProps } from "@chakra-ui/react";
 
-type Props = HeadingProps & { as: "h2" | "h3" | "h4" };
+type Props = HeadingProps & { as: "h2" | "h3" | "h4" | "h5" };
 
 const HeadlineElement = ({ as, ...props }: Props): ReactElement => (
   <>
@@ -12,7 +12,10 @@ const HeadlineElement = ({ as, ...props }: Props): ReactElement => (
       <Heading as="h3" fontSize="2.4rem" fontWeight="700" lineHeight="3rem" {...props} />
     )}
     {as === "h4" && (
-      <Heading as="h4" fontSize="2rem" fontWeight="400" lineHeight="2.6rem" {...props} />
+      <Heading as="h4" fontSize="2rem" fontWeight="700" lineHeight="2.6rem" {...props} />
+    )}
+    {as === "h5" && (
+      <Heading as="h5" fontSize="1.8rem" fontWeight="700" lineHeight="2.4rem" {...props} />
     )}
   </>
 );

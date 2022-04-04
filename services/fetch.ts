888e4f4267091d58fa0a08420a82525186278d35
Gabriel Mochi <gmochi56@icloud.com>
2022-04-04 14:01:56 -0300

    removes console.log

--- services/fetch.ts
+++ services/fetch.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { API_BASE_URL } from "configs/api.config";
 
-console.log(`${API_BASE_URL}/api/v1`);
-
 export const fetch = axios.create({
   baseURL: `${API_BASE_URL}/api/v1`,
   timeout: 60 * 1000,

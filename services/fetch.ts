7256ae6c0725437cb22b20083df21501285db7a9
Gabriel Mochi <gmochi56@icloud.com>
2022-04-04 13:46:31 -0300

    sets config for production deploy

--- services/fetch.ts
+++ services/fetch.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { API_BASE_URL } from "configs/api.config";
+
+console.log(`${API_BASE_URL}/api/v1`);
 
 export const fetch = axios.create({
-  baseURL: "/api/v1",
+  baseURL: `${API_BASE_URL}/api/v1`,
   timeout: 60 * 1000,
   withCredentials: true,
 });
